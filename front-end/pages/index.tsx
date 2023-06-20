2b20a341b57bb8cf97f8c3c8ac90c76d0c5e4518
Ruttiyaporn Kongtrakul <kongtrakulmint@gmail.com>
2023-06-20 17:17:03 +0700

    session show

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -5,13 +5,14 @@ import { Button } from "@mui/material";
 
 export default function Home() {
   const { status, data } = useSession();
-
+  const { data: session } = useSession();
   useEffect(() => {
     console.log(data);
     if (status === "unauthenticated") Router.replace("/signin");
   }, [status]);
   return (
     <div>
+      <p>role : {session ? session.user.user.role : "null"}</p>
       Index Page
       <Button onClick={() => signOut()}>Sign out</Button>
     </div>
